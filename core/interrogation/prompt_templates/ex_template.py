177860520eb7a8223efe054bad521cf504a49334
Hyrge <ekek7478@gmail.com>
2025-03-29 18:43:39 +0900

    chore: 템플릿 예시 추가가

---
+++ core/interrogation/prompt_templates/ex_template.py
@@ -0,0 +1,16 @@
+# 템플릿 정의
+GREETING_TEMPLATE = "Hello, {name}! How can I assist you today?"
+
+QUESTION_TEMPLATE = """
+You are a helpful assistant.
+
+Question: {question}
+Answer:
+"""
+
+FEEDBACK_TEMPLATE = """
+Provide feedback based on the following text:
+
+Text: {text}
+Feedback:
+"""
