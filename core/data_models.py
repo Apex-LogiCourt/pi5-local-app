b4091d2eb320b63599016df243bce3d37a44cf85
Hyrge <ekek7478@gmail.com>
2025-04-04 11:08:52 +0900

    feat: 사건 내막 및 출석 맥락에 대한 주석 추가, 증거품 이름 주석 수정

--- core/data_models.py
+++ core/data_models.py
@@ -8,23 +8,29 @@ from typing import List, Literal
 @dataclass
 class Case:
     outline: str  # 사건 개요
-    behind : str
+    behind : str  # 사건 내막 -- 사건의 진실 진짜 피고는 진범이었나?
 
 @dataclass
 class Profile:
     name: str  # 사람 이름
     type: Literal["witness", "reference", "defendant"]  # 참고인 유형
-    context: str  # 출석 맥락
+    context: str  # 출석 맥락 -- 어떤 사연으로 여기 출석하게 되었는지
 
 @dataclass
 class Evidence:
-    name: str  # 증거품 이름
+    name: str  # 증거품 이름(명사형) 
     type: Literal["attorney", "prosecutor"]  # 제출 주체
     description: List[str]  # 증거 설명 (추가 가능)
-    # picture: str  # 내부 사진 경로 (향후 구현)
+    # picture: str  # 사진 경로 (향후 구현)
 
+
+# Controller에서 최종적으로 다른 모듈로 념겨줄 데이터 형식이에용
+# 아직 구현이 안 됐지만 이렇게 넘어올거라고 믿고 작업해주세요 
 @dataclass
 class CaseData:
     case: Case
     profiles: List[Profile]
     evidences: List[Evidence]
+
+
+
