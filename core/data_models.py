4b0a3b1b234d568efdab868d713aaf1116e7d31b
Hyrge <ekek7478@gmail.com>
2025-04-04 11:04:31 +0900

    feat: 사건 및 프로필, 증거 데이터 저장을 위한 새로운 데이터클래스 추가

--- core/data_models.py
+++ core/data_models.py
@@ -1,16 +1,30 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Literal
 
 #==============================================
 # 데이터만 저장하기 위한 데이터클래스 선언 
 #==============================================
+
 @dataclass
-class WitnessProfile:
-    name: str
-    type: str
-    background: str
+class Case:
+    outline: str  # 사건 개요
+    behind : str
+
+@dataclass
+class Profile:
+    name: str  # 사람 이름
+    type: Literal["witness", "reference", "defendant"]  # 참고인 유형
+    context: str  # 출석 맥락
+
+@dataclass
+class Evidence:
+    name: str  # 증거품 이름
+    type: Literal["attorney", "prosecutor"]  # 제출 주체
+    description: List[str]  # 증거 설명 (추가 가능)
+    # picture: str  # 내부 사진 경로 (향후 구현)
 
 @dataclass
 class CaseData:
-    case_summary: str
-    witness_profiles: List[WitnessProfile]
+    case: Case
+    profiles: List[Profile]
+    evidences: List[Evidence]
