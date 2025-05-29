bebe888e6c5c3b89cb47199d755155692180e2cf
spq003 <spq002@naver.com>
2025-05-29 23:06:13 +0900

    feature: SSE 핸들링 로직 추가

--- hardware/main.py
+++ hardware/main.py
@@ -1,9 +1,13 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# TODO data_models.py 파일을 hw쪽에 복사해서 사용.
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import asyncio
-from api.http_request import websocket_client
+from api.http_request import websocket_client, listen_sse_async
 from devices import button_listener, rfid_reader
-import time
 from contextlib import asynccontextmanager
 
 @asynccontextmanager
@@ -11,11 +15,13 @@ async def lifespan(app: FastAPI):
     button_listener.button_init()
     task1 = asyncio.create_task(websocket_client())
     task2 = asyncio.create_task(rfid_reader.scan_rfid_loop())
+    task3 = asyncio.create_task(listen_sse_async())
     print("[lifespan] HW module initialized.")
     yield
     task1.cancel()
     task2.cancel()
-    await asyncio.gather(task1, task2, return_exceptions=True)
+    task3.cancel()
+    await asyncio.gather(task1, task2, task3, return_exceptions=True)
     rfid_reader.rfid_exit()
     button_listener.button_exit()
 
