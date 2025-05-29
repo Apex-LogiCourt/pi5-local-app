96106f3151be0b7f70afb83df7e676f564564572
spq003 <spq002@naver.com>
2025-05-29 23:03:39 +0900

    refactor: MAC table 로직 수정

--- hardware/devices/eink_display.py
+++ hardware/devices/eink_display.py
@@ -3,49 +3,52 @@ import serial
 import time
 import numpy as np
 import os
+import asyncio
 
 from PIL import Image, ImageDraw, ImageFont
-from enum import Enum
+from typing import List
 
-from typing import List, Literal        #pi 테스트용
-from dataclasses import dataclass       #pi 테스트용
-# from core.data_models import Evidence #나중에 사용
-@dataclass
-class Evidence:
-    name: str  # 증거품 이름(명사형) 
-    type: Literal["attorney", "prosecutor"]  # 제출 주체 
-    description: List[str]  # 증거 설명 (추가 가능)
-    picture: str  # 사진 경로 (향후 구현)
+# from typing import List, Literal        #pi 테스트용
+# from dataclasses import dataclass       #pi 테스트용
+from core.data_models import Evidence #나중에 사용
+# @dataclass
+# class Evidence:
+#     name: str  # 증거품 이름(명사형) 
+#     type: Literal["attorney", "prosecutor"]  # 제출 주체 
+#     description: List[str]  # 증거 설명 (추가 가능)
+#     picture: str  # 사진 경로 (향후 구현)
 
 RFCOMM_DEV = "/dev/rfcomm0" # Interface for Serial mapping of Bluetooth.
 
-class EPD_MacAddress(Enum):
-    EPD_01 = "EC:C9:FF:42:BA:C6"
-    EPD_02 = ""
-    EPD_03 = ""
-    EPD_04 = ""
+EPD_MacAddress = {
+    1: "EC:C9:FF:42:BA:C6",
+    2: "",
+    3: "",
+    4: ""
+}
 
 # EPD모듈 번호(1~4), 증거품 입력 시, 이미지 생성 후 해당 모듈에 전송
-def update_and_sand_image(epd_index, evidence: Evidence):
-    epd_mac = get_mac(epd_index)
-    bind_rfcomm(epd_mac)
-
-    img_path = make_epd_image(evidence)
-
-    byte_data = convert_image_to_bytes(img_path)
-    print(f"총 전송 크기: {len(byte_data)} bytes")
-    send_bytes_over_serial(byte_data)
+def update_and_sand_image(epd_index: int, evidence: Evidence):
+    try:
+        epd_mac = EPD_MacAddress[epd_index]
+        bind_rfcomm(epd_mac)
+        img_path = make_epd_image(evidence)
+        byte_data = convert_image_to_bytes(img_path)
+        print(f"총 전송 크기: {len(byte_data)} bytes")
+        send_bytes_over_serial(byte_data)
+    except Exception as e:
+        print(f"[HW/EPD] epd update error: {e}")
     return
 
 
 def bind_rfcomm(mac_addr): #바인딩 이후, RFCOMM_DEV를 USBSerial처럼 사용
     if not os.path.exists(RFCOMM_DEV):
-        print(f"{RFCOMM_DEV} 바인딩 시도...")
+        print(f"[HW/EPD]{RFCOMM_DEV} 바인딩 시도...")
         try:
             subprocess.run(["sudo", "rfcomm", "bind", "0", mac_addr, "1"], check=True)
-            print("rfcomm 바인딩 완료")
+            print("[HW/EPD]rfcomm 바인딩 완료")
         except subprocess.CalledProcessError as e:
-            print("rfcomm 바인딩 실패:", e)
+            print("[HW/EPD]rfcomm 바인딩 실패:", e)
     else:
         print(f"{RFCOMM_DEV} 이미 바인딩되어 있음")
     time.sleep(0.5) # 바인딩 안정화용 지연
@@ -59,7 +62,7 @@ def convert_image_to_bytes(image_path):
 def send_bytes_over_serial(byte_data):
     try:
         with serial.Serial(RFCOMM_DEV, baudrate=115200, timeout=1) as ser:
-            print("시리얼 포트 열림, 데이터 전송 중...")
+            print("[HW/EPD]시리얼 포트 열림, 데이터 전송 중...")
             for i in range(0, len(byte_data), 512):
                 chunk = byte_data[i:i+512].tobytes()
                 ser.write(chunk)
@@ -68,17 +71,12 @@ def send_bytes_over_serial(byte_data):
             ser.flush()
             time.sleep(0.2)
             ser.close()
-            print("전송 완료")
+            print("[HW/EPD]전송 완료")
     except serial.SerialException as e:
-        print("시리얼 포트 오류:", e)
+        print("[HW/EPD]시리얼 포트 오류:", e)
 
 def get_evidence_image_path(e: Evidence):
     return e.picture
-
-def get_mac(index):
-    mac_list = list(EPD_MacAddress)
-    return mac_list[index-1].value
-
 
 #========== EPD용 이미지 생성 ==========
 font_path = "/home/user/Desktop/fonts/NanumGothic.ttf" #pi 테스트용
@@ -124,13 +122,11 @@ def make_epd_image(evidence: Evidence, font_size=20, line_spacing=6):
         y += font_size + line_spacing #줄간격 설정
 
     canvas.save(save_path)
-    print(f"기본 이미지 생성 완료: {save_path}")
+    print(f"[HW/EPD]기본 이미지 생성 완료: {save_path}")
 
     if len(evidence.description) > 1:
         update_epd_image(save_path, evidence)
-
-        print("이미지 설명 추가 완료")
-
+        print("[HW/EPD]이미지 설명 추가 완료")
     return (save_path)
 
 
@@ -170,7 +166,7 @@ def update_epd_image(image_path, evidence: Evidence, font_size=20, line_spacing=
         y += font_size + line_spacing
 
     img.save(image_path)
-    print(f"저장 완료: {image_path}")
+    print(f"[HW/EPD]저장 완료: {image_path}")
     return 
 
 
