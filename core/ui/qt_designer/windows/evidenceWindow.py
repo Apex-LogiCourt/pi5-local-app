37982a0d32aff8cfa3f54cdc26b5d571b8e2f563
spq003 <spq002@naver.com>
2025-11-16 18:38:52 +0900

    feat: 증거품 창 이미지 구현

--- core/ui/qt_designer/windows/evidenceWindow.py
+++ core/ui/qt_designer/windows/evidenceWindow.py
@@ -1,23 +1,25 @@
 import sys
 import os
 from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.QtGui import QPixmap
 from PyQt5 import uic
 from typing import List
 from data_models import Evidence 
 
 class EvidenceWindow(QDialog):
     """생성자에 evidence 리스트랑 부모 윈도우 받기"""
-    def __init__(self, evidences: List, parent=None):
+    def __init__(self, evidences: List[Evidence], parent=None):
         super(EvidenceWindow, self).__init__(parent)
         
         # UI 파일 로드
         ui_path = os.path.join(os.path.dirname(__file__), '..', 'evidenceWindow.ui')
         uic.loadUi(ui_path, self)
 
+        
         self.setWindowTitle("증거품 창")
         self._display_evidences(evidences)
         
-    def _display_evidences(self, evidences: List):
+    def _display_evidences(self, evidences: List[Evidence]):
         """증거품 리스트를 UI에 표시"""
         evidence_labels = [
             self.evidenceLabel1,
@@ -33,9 +35,24 @@ class EvidenceWindow(QDialog):
             self.evidenceDescription4
         ]
 
+        evidence_images = [
+            self.evidenceImage1,
+            self.evidenceImage2,
+            self.evidenceImage3,
+            self.evidenceImage4
+        ]
+
+        default_path = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..')
+
         for i, evidence in enumerate(evidences):
             evidence_labels[i].setText(evidence.name)
             evidence_descriptions[i].setText("\n".join(evidence.description))
+
+            image_path = default_path + "/" + str(evidence.picture)
+            if not(os.path.exists(image_path)):
+                image_path = default_path + "/data/evidence_resource/deafult_image.png"
+            evidence_images[i].setPixmap(QPixmap(f"{image_path}").scaled(100, 100))
+            
         
 # 테스트용 메인 함수
 if __name__ == "__main__":
